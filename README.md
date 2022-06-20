c025d0cfcc309e22f585865be635cd9e34b29a34
Luc Anise <lca11@me.com>
2022-06-20 00:14:23 -0400

    finished

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Homework 3
 
 ## Question 1
+To create the mandlebrot call the following function:
+```python
+madelbrot()
+```
+It takes 3 optional arguments `n`, `N_max`, and `threshold` and saves the output a file called `mandlebrot.png`.
+If no arguments are passed the functions default values are `n=1000` `N_max=50` and `threshold=50`.
 
 ## Question 2
-To run the markov chain call the following function with values for `n` and `N`:
+To run the markov chain call the following function with values for `n` and `N` where `n` is the length if p in the chain and `N` is the size of P.
 ```python 
 markov_chain(n, N)
 ```
--- mandelbrot.py
+++ mandelbrot.py
@@ -1,39 +1,34 @@
-# import numpi as np
 import numpy as np
-# construct an n×n grid (2D array) of points (x, y) in range [−2, 1]×[−1.5, 1.5] (e.g., using numpy.meshgrid)
-grid = np.meshgrid(np.linspace(-2, 1, 100), np.linspace(-1.5, 1.5, 100))
-
- #and corresponding complex values c = x + yi (note that the imaginary unit in Python is 1j);
-# perform the iteration as outlined above to compute z for each complex value in the grid;
-# store the result in a 2D array (the same shape as c)
-mask = np.zeros(grid.shape)
-for  x, y in zip(grid[0], grid[1]):
-    c = x + y * 1j
-    z = 0
-    N_max = 50
-    threshold = 50
-    for n in range(N_max):
-        z = z**2 + c
-        if abs(z) > threshold:
-            mask[n] = 1
-            break
-# form a 2-D boolean array mask indicating which points are in the set (i.e., |z| < threshoold)
-mask = np.abs(z) < threshold
-
-# import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
 
-# plot the grid and the mask
-plt.imshow(mask, extent=[-2, 1, -1.5, 1.5], cmap='gray')
-plt.gray()
-plt.show()
+plt.close('all')
 
-""" # organize the statements into a function that takes n, N_max, and threshold as input, add docstring and comments to the function, and experiment the function with different n so that you get an image resembling the one above.
-def mandelbrot(n, N_max, threshold):
+def madelbrot(n=1000, N_max=50, threshold=50):
+    """A function that takes n, N_max and threshold as input computes the madelbrot set."""
+    # construct an n×n grid (2D array) of points (x, y) in range [−2, 1]×[−1.5, 1.5] (e.g., using numpy.meshgrid)
+    # and corresponding complex values c = x + yi (note that the imaginary unit in Python is 1j);
     grid = np.meshgrid(np.linspace(-2, 1, n), np.linspace(-1.5, 1.5, n))
-    c = grid[0] + grid[1] * 1j
-    z = 0
-    for j in range (N_max ) :
+
+    #  corresponding complex values c = x + yi (note that the imaginary unit in Python is 1j);
+    c = grid[0] + 1j * grid[1]
+
+    # initialize the array z to have the same shape as c
+    z = np.zeros(c.shape, dtype=complex)
+    
+    # caculate the Mandelbrot set using the formula z = z^2 + c
+    # and store the result in the array z
+    for i in range(N_max):
         z = z**2 + c
+    #  form a 2-D boolean array mask indicating which points are in the set (i.e., |z| < threshoold);
     mask = np.abs(z) < threshold
-    return mask """
+
+    # plot the Mandelbrot set using matplotlib.pyplot.imshow
+    plt.imshow(mask.T, extent=[-2, 1, -1.5 ,1.5])
+    plt.gray()
+    plt.show()
+    plt.savefig('mandelbrot.png')
+    return z
+
+
+
+madelbrot(1000, 50, 50)
